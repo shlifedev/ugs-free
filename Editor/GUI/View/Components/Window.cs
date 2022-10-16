7c9f14e5a103627ac5d6e5c35d180952d15b52a5
shlifedev <shlifedev@gmail.com>
2022-10-17 03:21:28 +0900

    automatic queried using uquery attributes

--- Editor/GUI/View/Components/Window.cs
+++ Editor/GUI/View/Components/Window.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UGS.Editor;
+using LifeDev.UIElementsExtension;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace UGS.Editor
 {
-    internal class Window : ElementTreeAssetBase<VisualElement>
+    public class Window : ElementTreeAssetBase<VisualElement>
     {
+        [UQuery("window-pwd-container")]
+        public VisualElement PwdContainer;
         public override string ComponentPath => "GUI/View/Components/";
 
         public Window(VisualElement target) : base(target)
-        {
+        { 
+           
         } 
     }
 }
