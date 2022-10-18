4c56629759f8a13d3148ad50c3ea51b05d9b99f6
Hamster CheeseAllergy <shlifedev@gmail.com>
2022-10-18 23:20:13 +0900

    arrange unused

--- Runtime/Core/GeneralDataDictionary.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace UGS.Runtime.Core
-{
-    public class GeneralDataDictionary : Dictionary<string, object>
-    {
-    }
-}
--- Runtime/Core/SettingData.cs
+++ Runtime/Core/SettingData.cs
@@ -1,8 +1,21 @@
 ﻿namespace UGS.Runtime
-{
-    internal abstract class SettingData
+{ 
+    [System.Serializable]
+    public class SettingData
     {
-        public abstract string DriveId { get; set; }
-        public abstract string ScriptUrl { get; set; }
+        public SettingData(string driveId, string scriptDeploymentId)
+        {
+            DriveId = driveId;
+            ScriptDeploymentId = scriptDeploymentId;
+        }
+
+        /// <summary>
+        /// Drive Id
+        /// </summary>
+        public string DriveId { get; set; }
+        /// <summary>
+        /// 스크립트가 배포된 Id 
+        /// </summary>
+        public string ScriptDeploymentId { get; set; }
     }
 }
